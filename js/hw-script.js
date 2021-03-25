d133d31c6717b7452f5fbcb163e1f59c7d7a2a92
2F4S7 <nightsta1kerhf@gmail.com>
2021-03-25 11:21:38 +0200

    JS-HW-08

--- js/hw-script.js
+++ js/hw-script.js
@@ -44,6 +44,96 @@ function createElements({ original, preview, descreption }) {
   liElement.appendChild(referense);
 
   return liElement;
-};
+}
 
-function 
+function handleNewElements(images) {
+  return images.map(createElements);
+}
+
+function handleRenderElements(newElements) {
+  galleryRef.append(...newElements);
+}
+
+handleRenderElements(handleNewElements(gallery));
+
+function handleModalOpen(event) {
+  event.preventDefault();
+  window.addElementListener("keydown", handlePressKey);
+
+  const { target } = event;
+  const { dataset } = target;
+
+  if (target.nodeName != "IMG") {
+    return;
+  }
+
+  const hrefImg = dataset.source;
+  const indexCount = dataset.index;
+
+  modalRef.classList.add("is-open");
+  bigImgRef.src = hrefImg;
+  bigImgRef.alt = target.alt;
+  bigImgRef.setAttribute("data-index", indexCount);
+}
+
+function handleModalClose() {
+  modalRef.classList.remove("is-open");
+  bigImgRef.src = "";
+  bigImgRef.alt = "";
+  window.removeEventListener("keydown", handlePressKeys);
+}
+
+function handleBackdropCloseModal({ target, currentTarget }) {
+  if (target === currentTarget) {
+    handleModalClose();
+  }
+}
+
+function handleChangeClick({ target }) {
+  const { dataset } = target;
+  const currentIndexImg = Number(dataset.index);
+
+  if (currentIndexImg + 1 === gallery.length) {
+    target.src = gallery[0].original;
+    target.alt = gallery[0].description;
+    dataset.index = 0;
+    return;
+  }
+
+  target.src = gallery[currentIndexImg + 1].original;
+  target.alt = gallery[currentIndexImg + 1].description;
+  dataset.index = currentIndexImg + 1;
+}
+
+function handlePressKeys(event) {
+  if (event.code === "Escape") {
+    handleModalClose();
+  }
+
+  const { dataset } = bigImgRef;
+  const index = Number(dataset.index);
+
+  if (event.code === "ArrowRight") {
+    if (index + 1 === gallery.length) {
+      bigImgRef.src = gallery[0].original;
+      bigImgRef.alt = gallery[0].description;
+      dataset.index = 0;
+      return;
+    }
+    bigImgRef.src = gallery[index + 1].original;
+    bigImgRef.alt = gallery[index + 1].description;
+    dataset.index = index + 1;
+  }
+
+  if (event.code === "ArrowLeft") {
+    if (index - 1 < 0) {
+      bigImgRef.src = gallery[8].original;
+      bigImgRef.src = gallery[8].description;
+      dataset.index = 8;
+      return;
+    }
+    bigImgRef.src = gallery[index - 1].original;
+    bigImgRef.alt = gallery[index - 1].description;
+    dataset.index = index - 1;
+  }
+}
